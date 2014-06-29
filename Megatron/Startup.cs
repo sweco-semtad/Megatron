2f34d5342c392798ade8d10badae81625ced43bd
sweco-semtad <martin.andersson@sweco.se>
2014-06-29 20:49:12 +0200

    MVC template

---
+++ Megatron/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Megatron.Startup))]
+namespace Megatron
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
